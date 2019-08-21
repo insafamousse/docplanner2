1ce2fc60e976be553b722a514bcb0cb5451f6676
unknown <you@example.com>
2019-08-21 13:32:27 +0200

    Updates

---
+++ precache-manifest.a7c43fb86ddcf57ad6288e1075e84d30.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630ab4aa4b0916877628240e88c20be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2ead2727fece7db0b4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3798e1f3fd0f9802c991",
+    "url": "/static/css/main.2550e5fe.chunk.css"
+  },
+  {
+    "revision": "6c2ead2727fece7db0b4",
+    "url": "/static/js/2.e2540bd1.chunk.js"
+  },
+  {
+    "revision": "3798e1f3fd0f9802c991",
+    "url": "/static/js/main.14227715.chunk.js"
+  },
+  {
+    "revision": "fa28b596c9eede69bbb8",
+    "url": "/static/js/runtime~main.784842b7.js"
+  },
+  {
+    "revision": "152f007cb7e202b072209d1c95eb076b",
+    "url": "/static/media/barcelona.152f007c.png"
+  },
+  {
+    "revision": "a9ff15a2b335713338f444d604c047eb",
+    "url": "/static/media/curitiba.a9ff15a2.png"
+  },
+  {
+    "revision": "81a5e4ff04902657121eff83d69a4070",
+    "url": "/static/media/image.81a5e4ff.png"
+  },
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/static/media/img.120076c7.png"
+  },
+  {
+    "revision": "40d3772597883d2e2b04e59087633ba2",
+    "url": "/static/media/istanbul.40d37725.png"
+  },
+  {
+    "revision": "47ca4c93d3fb1d4d6f655284da529ef6",
+    "url": "/static/media/mexico.47ca4c93.png"
+  },
+  {
+    "revision": "4617f89b0fe16a5ff42dc353495dbdd4",
+    "url": "/static/media/rome.4617f89b.png"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/static/media/screen 1.82d323de.png"
+  },
+  {
+    "revision": "9d829d85c292405acbb619a69aa1c7fa",
+    "url": "/static/media/warsaw.9d829d85.png"
+  }
+]);
